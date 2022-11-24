54beef3c179dd43486551b8a208057b4aa9a469b
psalmseen <samsonoyebamiji02@gmail.com>
2022-11-24 01:19:19 +0100

    new

--- sample.test.ts
+++ sample.test.ts
@@ -2,6 +2,7 @@ import { it, expect } from 'vitest';
 it('is a sample', () => {
   expect(true).toBeTruthy();
   expect(false).toBeFalsy();
+  expect(false).toBeFalsy();
   expect(1).toBeGreaterThan(0);
   expect(1).toBeGreaterThan(0);
 });
