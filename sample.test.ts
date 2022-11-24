a826426d29adf7b1069caf91b073d13a40ef335b
psalmseen <samsonoyebamiji02@gmail.com>
2022-11-24 12:41:53 +0100

    pullrequest feature done

--- sample.test.ts
+++ sample.test.ts
@@ -1,8 +1,6 @@
 import { it, expect } from 'vitest';
 it('is a sample', () => {
   expect(true).toBeTruthy();
-
-  expect(true).toBeTruthy();
   expect('Hanifah to be ').toBeTypeOf('string');
   expect(31).toBeGreaterThan(0);
 });
